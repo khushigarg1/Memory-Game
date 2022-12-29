b4624dc39dec29c288d4f73520daa52aeb1c2400
khushigarg1 <khushigarg.64901@gmail.com>
2022-12-30 02:32:13 +0530

    centerd div

--- src/App.js
+++ src/App.js
@@ -79,15 +79,18 @@ function App() {
     return (
         <>
             <Background />
-            <h1>Memory Game</h1>
-            <button onClick={shuffleCards}>NEW GAME</button>
+            <div className='app'>
 
-            <div className="card-grid" >
-                {cards.map(card => (
-                    <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched} disabled={disabled} />
-                ))}
+                <h1>Memory Game</h1>
+                <button onClick={shuffleCards}>NEW GAME</button>
+
+                <div className="card-grid" >
+                    {cards.map(card => (
+                        <SingleCard key={card.id} card={card} handleChoice={handleChoice} flipped={card === choiceOne || card === choiceTwo || card.matched} disabled={disabled} />
+                    ))}
+                </div>
+                <p>Turns: {turns}</p>
             </div>
-            <p>Turns: {turns}</p>
         </>
     );
 }
